0fb3e27feee84b8379f841aec91f6efe616783c7
goenawanjodhi <goenawanjodhi@gmail.com>
2023-10-20 17:42:24 +0700

    cross origin allow for app todo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5002;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://app-todo-iota.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://app-todo-drab.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
