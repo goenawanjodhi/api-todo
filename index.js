b079671ae214a90a1327bfd667303cd2152facc6
goenawanjodhi <93862485+goenawanjodhi@users.noreply.github.com>
2023-10-20 18:11:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5002;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://app-todo-six.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://app-todo-six.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
